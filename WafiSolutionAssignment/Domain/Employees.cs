9b16494d5bff7ec4658635cceb739ce2950a2f84
Atikul1213 <atikul.islam@brainstation-23.com>
2024-09-12 21:57:18 +0600

    Add project files.

---
+++ WafiSolutionAssignment/Domain/Employees.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WafiSolutionAssignment.Domain
+{
+    public class Employees
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string MobileNumber { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
---
+++ WafiSolutionAssignment/Models/EmployeeModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WafiSolutionAssignment.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+        [DisplayName("Mobile Number")]
+        public string MobileNumber { get; set; }
+        [DisplayName("Image")]
+
+        public string ImageUrl { get; set; }
+        [DisplayName("Email")]
+        public string Email { get; set; }
+    }
+}
